f7bad00ad23a7e064839dce00741596aeb456c6f
anhkynguyen <anhkynguyen1994@gmail.com>
2023-02-27 10:15:18 +0700

    kybe

--- src/model/home.ts
+++ src/model/home.ts
@@ -1,24 +1,24 @@
 import { text } from "express";
-import {Column, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
-import { Image } from "./image";
-
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Home {
-    @PrimaryGeneratedColumn()
-    idHome: number;
-    @Column()
-    nameHome: string;
-    @Column()
-    address: string
-    @Column()
-    description: string;
-    @Column()
-    price: number;
-    @Column()
-    idCategory: number;
-    @Column({default: 0})
-    count: number;
-    @Column()
-    idUser: number;
+  @PrimaryGeneratedColumn()
+  idHome: number;
+  @Column()
+  nameHome: string;
+  @Column()
+  address: string;
+  @Column()
+  description: string;
+  @Column()
+  price: number;
+  @Column()
+  idCategory: number;
+  @Column()
+  image: string;
+  @Column({ default: 0 })
+  count: number;
+  @Column()
+  idUser: number;
 }
